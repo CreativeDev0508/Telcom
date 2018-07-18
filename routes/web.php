42ce76baaf31b6bc4521d099b7a788bb5567dc2f
Luckyman909 <mingw2816@gmail.com>
2018-07-18 11:53:20 +0700

    update unit kerja

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::post('/AM-form-aspek/insert','AMController@insertAspek');
 
 Route::get('/AM-unit-kerja','AMController@indexUnitKerja');
 Route::post('/AM-unit-kerja/insert','AMController@insertUnitKerja');
+Route::get('/AM-unit-kerja/update/{id}', 'AMController@updateUnitKerja');
 
 Route::get('/AM-mitra','AMController@indexMitra');
 Route::post('/AM-mitra/insert','AMController@insertMitra');
