e003307853f895ecf19e41c42859aee465591e38
Luckyman909 <mingw2816@gmail.com>
2018-07-18 17:35:52 +0700

    fix unit-kerja

--- resources/views/AM/unit-kerja.blade.php
+++ resources/views/AM/unit-kerja.blade.php
@@ -14,52 +14,51 @@
                         <div class="white-box">
                             <h1 class="text-center" style="color: #d51100; font-weight: 500">UNIT KERJA</h1>
                             <button type="button" class="btn btn-danger btn-rounded" style="background-color: #d51100;" data-toggle="modal" data-target="#tambah-unit">Tambah Unit Kerja</button>
-                                <div class="modal fade" id="tambah-unit" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
-                                    <div class="modal-dialog modal-lg">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h4 class="modal-title" id="myLargeModalLabel">Tambah Unit Kerja</h4>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form class="form-horizontal form-material" action="{{ url('/AM-unit-kerja/insert') }}" method = "post">
-                                                    {{ csrf_field() }}
-                                                    <div class="form-group">
-                                                        <label for="inputEmail3" class="col-sm-3 control-label">Nama Unit Kerja</label>
-                                                        <div class="col-sm-9">
-                                                            <input type="text" class="form-control" id="inputEmail3" placeholder="Nama Unit Kerja" name="nama_unit_kerja">
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="inputEmail3" class="col-sm-3 control-label">Deskripsi Unit Kerja</label>
-                                                        <div class="col-sm-9">
-                                                            <textarea class="form-control" rows="5" placeholder="Deskripsi Unit Kerja" name="deskripsi_unit_kerja"></textarea>
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group m-b-0">
-                                                        <a href="#" class="fcbtn btn btn-default btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right;">Keluar</a>
-                                                        <button type="submit" style="float: right;" class="btn btn-danger waves-effect waves-light m-t-10">Simpan</button>
-                                                    </div>
-                                               </form>
-                                            </div>
+                            <div class="modal fade" id="tambah-unit" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
+                                <div class="modal-dialog modal-lg">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="myLargeModalLabel">Tambah Unit Kerja</h4>
                                         </div>
-                                        <!-- /.modal-content -->
+                                        <div class="modal-body">
+                                            <form class="form-horizontal form-material" action="{{ url('/AM-unit-kerja/insert') }}" method = "post">
+                                                {{ csrf_field() }}
+                                                <div class="form-group">
+                                                    <label for="inputEmail3" class="col-sm-3 control-label">Nama Unit Kerja</label>
+                                                    <div class="col-sm-9">
+                                                        <input type="text" class="form-control" id="inputEmail3" placeholder="Nama Unit Kerja" name="nama_unit_kerja">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputEmail3" class="col-sm-3 control-label">Deskripsi Unit Kerja</label>
+                                                    <div class="col-sm-9">
+                                                        <textarea class="form-control" rows="5" placeholder="Deskripsi Unit Kerja" name="deskripsi_unit_kerja"></textarea>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group m-b-0">
+                                                    <a href="#" class="fcbtn btn btn-default btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right;">Keluar</a>
+                                                    <button type="submit" style="float: right;" class="btn btn-danger waves-effect waves-light m-t-10">Simpan</button>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
-                                    <!-- /.modal-dialog -->
                                 </div>
-                                <table id="demo-foo-accordion" class="table m-b-0 toggle-arrow-tiny">
-                                    <thead>
-                                        <tr>
-                                            <th data-toggle="true"> Unit Kerja </th>
-                                            <th data-hide="all"> Deskripsi </th>
-                                            <th data-hide="all"> Edit </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($unit_kerja as $listunit_kerja)
-                                        <tr>
-                                            <td>{{$listunit_kerja->nama_unit_kerja}}</td>
-                                            <td>{{$listunit_kerja->deskripsi_unit_kerja}}</td>
-                                            <td>
+                            </div>
+                                
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th style="text-align: center;">Unit Kerja</th>
+                                        <th style="text-align: center;">Deskripsi</th>
+                                        <th style="text-align: center; width: 15%;">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($unit_kerja as $listunit_kerja)
+                                    <tr>
+                                        <td align="center" style="width: 9%;">{{$listunit_kerja->nama_unit_kerja}}</td>
+                                        <td style="text-align: justify;">{{$listunit_kerja->deskripsi_unit_kerja}}</td>
+                                        <td>
                                             <br>
                                             <button type="submit" class="btn btn-danger btn-outline btn-circle btn-lg m-r-5" data-toggle="modal" data-target="#delete-{{$listunit_kerja->id_unit_kerja}}"><i class="ti-trash"></i></button>
                                             <div class="modal fade" id="delete-{{$listunit_kerja->id_unit_kerja}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
@@ -69,8 +68,8 @@
                                                             <h4 class="modal-title" id="myLargeModalLabel">Delete {{$listunit_kerja->nama_unit_kerja}}</h4> 
                                                         </div>
                                                         <div class="modal-body">
-                                                            <form class="form-horizontal form-material" action="{{ url('/AM-unit-kerja/delete/'.$listunit_kerja->id_unit_kerja) }}" method = "get"">
-                                                            <h5> Anda yakin untuk menghapus unit kerja "{{$listunit_kerja->nama_unit_kerja}}"? </h5>
+                                                            <form class="form-horizontal form-material" action="{{ url('/AM-unit-kerja/delete/'.$listunit_kerja->id_unit_kerja) }}" method = "get">
+                                                                <h5> Anda yakin untuk menghapus unit kerja "{{$listunit_kerja->nama_unit_kerja}}"? </h5>
                                                                 <div class="form-group m-b-0">
                                                                     <a href="#" class="fcbtn btn btn-default btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right;">Keluar</a>
                                                                     <button type="submit" style="float: right;" class="btn btn-danger waves-effect waves-light m-t-10">Hapus</button>
@@ -81,13 +80,13 @@
                                                 </div>
                                             </div>
                                             
-
                                             <button type="button" class="btn btn-danger btn-outline btn-circle btn-lg m-r-5" data-toggle="modal" data-target="#edit-{{$listunit_kerja->id_unit_kerja}}"><i class="ti-pencil-alt"></i></button>
                                             <div class="modal fade" id="edit-{{$listunit_kerja->id_unit_kerja}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
                                                 <div class="modal-dialog modal-lg">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <h4 class="modal-title" id="myLargeModalLabel">Edit {{$listunit_kerja->nama_unit_kerja}}</h4> </div>
+                                                            <h4 class="modal-title" id="myLargeModalLabel">Edit {{$listunit_kerja->nama_unit_kerja}}</h4>
+                                                        </div>
                                                         <div class="modal-body">
                                                             <form class="form-horizontal form-material" action="{{ url('/AM-unit-kerja/update/'.$listunit_kerja->id_unit_kerja) }}" method = "get">
                                                                 <div class="form-group">
@@ -102,16 +101,14 @@
                                                                         <textarea class="form-control" rows="5" name="deskripsi_unit_kerja">{{$listunit_kerja->deskripsi_unit_kerja}}</textarea>
                                                                     </div>
                                                                 </div>
-                                                                 <div class="form-group m-b-0">
+                                                                <div class="form-group m-b-0">
                                                                     <a href="#" class="fcbtn btn btn-default btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right;">Keluar</a>
                                                                     <button type="submit" style="float: right;" class="btn btn-danger waves-effect waves-light m-t-10">Simpan</button>
                                                                 </div>
-                                                           </form>
+                                                            </form>
                                                         </div>
                                                     </div>
-                                                    <!-- /.modal-content -->
                                                 </div>
-                                                <!-- /.modal-dialog -->
                                             </div>
                                         </td>
                                     </tr>
