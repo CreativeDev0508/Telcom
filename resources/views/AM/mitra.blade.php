2d63eef86b9621bc89522dc66d26db5b61b8042c
Luckyman909 <mingw2816@gmail.com>
2018-07-18 09:57:42 +0700

    show mitra

--- resources/views/AM/mitra.blade.php
+++ resources/views/AM/mitra.blade.php
@@ -54,9 +54,10 @@
                                     </tr>
                                 </thead>
                                 <tbody>
+                                @foreach($mitra as $listmitra)
                                     <tr>
-                                        <td>Isidra</td>
-                                        <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium mollitia doloribus, doloremque fugit distinctio quaerat expedita temporibus dolorum impedit accusantium officia, eos nam corrupti earum neque. Commodi, delectus blanditiis?</td>
+                                        <td>{{$listmitra->nama_mitra}}</td>
+                                        <td>{{$listmitra->deskripsi_mitra}}</td>
                                         <td>
                                             <br>
                                             <button type="button" class="btn btn-danger btn-outline btn-circle btn-lg m-r-5"><i class="ti-trash"></i></button>
@@ -71,13 +72,13 @@
                                                                 <div class="form-group">
                                                                     <label for="inputEmail3" class="col-sm-3 control-label">Nama Mitra</label>
                                                                     <div class="col-sm-9">
-                                                                        <input type="text" class="form-control" id="inputEmail3" placeholder="Daniel Kristeen">
+                                                                        <input type="text" class="form-control" id="inputEmail3" placeholder="{{$listmitra->nama_mitra}}">
                                                                     </div>
                                                                 </div>
                                                                 <div class="form-group">
                                                                     <label for="inputEmail3" class="col-sm-3 control-label">Deskripsi Mitra</label>
                                                                     <div class="col-sm-9">
-                                                                        <textarea class="form-control" rows="5" placeholder="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis ad temporibus iusto? Neque a quis veniam quibusdam, beatae cupiditate officia aliquid excepturi illo nemo, similique, est pariatur veritatis. Natus, tenetur."></textarea>
+                                                                        <textarea class="form-control" rows="5" placeholder="{{$listmitra->deskripsi_mitra}}"></textarea>
                                                                     </div>
                                                                 </div>
                                                                 <div class="form-group m-b-0">
@@ -93,84 +94,7 @@
                                             </div>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <td>Shona</td>
-                                        <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium mollitia doloribus, doloremque fugit distinctio quaerat expedita temporibus dolorum impedit accusantium officia, eos nam corrupti earum neque. Commodi, delectus blanditiis?</td>
-                                        <td>
-                                            <br>
-                                            <button type="button" class="btn btn-danger btn-outline btn-circle btn-lg m-r-5"><i class="ti-trash"></i></button>
-                                            <button type="button" class="btn btn-danger btn-outline btn-circle btn-lg m-r-5" data-toggle="modal" data-target="#mitra-2"><i class="ti-pencil-alt"></i></button>
-                                            <div class="modal fade" id="mitra-2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
-                                                  <div class="modal-dialog modal-lg">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h4 class="modal-title" id="myLargeModalLabel">edit #1</h4> </div>
-                                                        <div class="modal-body">
-                                                            <form class="form-horizontal form-material">
-                                                                <div class="form-group">
-                                                                    <label for="inputEmail3" class="col-sm-3 control-label">Nama Mitra</label>
-                                                                    <div class="col-sm-9">
-                                                                        <input type="text" class="form-control" id="inputEmail3" placeholder="Daniel Kristeen">
-                                                                    </div>
-                                                                </div>
-                                                                <div class="form-group">
-                                                                    <label for="inputEmail3" class="col-sm-3 control-label">Deskripsi Mitra</label>
-                                                                    <div class="col-sm-9">
-                                                                        <textarea class="form-control" rows="5" placeholder="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis ad temporibus iusto? Neque a quis veniam quibusdam, beatae cupiditate officia aliquid excepturi illo nemo, similique, est pariatur veritatis. Natus, tenetur."></textarea>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="form-group m-b-0">
-                                                                    <a href="#" class="fcbtn btn btn-default btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right;">Keluar</a>
-                                                                    <a href="#" class="fcbtn btn btn-danger btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right; background: #d51100; border: #d51100;">Simpan</a>
-                                                                </div>
-                                                           </form>
-                                                        </div>
-                                                    </div>
-                                                    <!-- /.modal-content -->
-                                                </div>
-                                                <!-- /.modal-dialog -->
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Granville</td>
-                                        <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium mollitia doloribus, doloremque fugit distinctio quaerat expedita temporibus dolorum impedit accusantium officia, eos nam corrupti earum neque. Commodi, delectus blanditiis?</td>
-                                        <td>
-                                                <br>
-                                                <button type="button" class="btn btn-danger btn-outline btn-circle btn-lg m-r-5"><i class="ti-trash"></i></button>
-                                                <button type="button" class="btn btn-danger btn-outline btn-circle btn-lg m-r-5" data-toggle="modal" data-target="#mitra-3"><i class="ti-pencil-alt"></i></button>
-                                                <div class="modal fade" id="mitra-3" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
-                                                      <div class="modal-dialog modal-lg">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h4 class="modal-title" id="myLargeModalLabel">edit #1</h4> </div>
-                                                            <div class="modal-body">
-                                                                <form class="form-horizontal form-material">
-                                                                    <div class="form-group">
-                                                                        <label for="inputEmail3" class="col-sm-3 control-label">Nama Mitra</label>
-                                                                        <div class="col-sm-9">
-                                                                            <input type="text" class="form-control" id="inputEmail3" placeholder="Daniel Kristeen">
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label for="inputEmail3" class="col-sm-3 control-label">Deskripsi Mitra</label>
-                                                                        <div class="col-sm-9">
-                                                                            <textarea class="form-control" rows="5" placeholder="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis ad temporibus iusto? Neque a quis veniam quibusdam, beatae cupiditate officia aliquid excepturi illo nemo, similique, est pariatur veritatis. Natus, tenetur."></textarea>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group m-b-0">
-                                                                        <a href="#" class="fcbtn btn btn-default btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right;">Keluar</a>
-                                                                        <a href="#" class="fcbtn btn btn-danger btn-1f m-r-10 m-t-10" data-dismiss="modal" style="padding-top: 5.5px; padding-bottom: 5.5px; float: right; background: #d51100; border: #d51100;">Simpan</a>
-                                                                    </div>
-                                                               </form>
-                                                            </div>
-                                                        </div>
-                                                        <!-- /.modal-content -->
-                                                    </div>
-                                                    <!-- /.modal-dialog -->
-                                                </div>
-                                            </td>
-                                    </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
